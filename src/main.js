38d2c8bb2288a874867501bad51aed3039e30f30
Miguel Santos <msantos11@ucol.mx>
2020-12-11 14:52:36 -0600

    implemented obtenerMultiplos

--- src/main.js
+++ src/main.js
@@ -27,7 +27,42 @@ export default class App {
     } while (i < numero);
     return primo;
   }
+  obtenerMultiplos(inicio, fin) {
+    let i = inicio;
+    let multiplos = "";
+
+    do {
+      if (i % 3 == 0) {
+        multiplos = multiplos + i;
+      }
+      i++;
+    } while (i <= fin);
+    return multiplos;
+  }
+  obtenerImpares(numero1, numero2) {
+    let impares = 0;
+    let t;
+
+    if (numero1 > numero2) {
+      t = numero1;
+      numero1 = numero2;
+      numero2 = t;
+    }
+    for (let i = numero2; i >= numero1; i--) {
+      if (i % 2 != 0) {
+        if (impares == 0) {
+          impares = impares + i;
+          impares = impares.toString();
+        } else {
+          impares = impares + "," + i;
+          impares = impares.toString();
+        }
+      }
+    }
+    return impares;
+  }
 }
+
 let app = new App();
 
 console.log(app.sumatoriaSerieUno(5));
@@ -37,3 +72,7 @@ console.log(app.sumatoriaSerieDos(5));
 console.log(app.esPrimo(3));
 
 console.log(app.esPrimo(4));
+
+console.log(app.obtenerMultiplos(1, 9));
+
+console.log(app.obtenerImpares(1, 9));
